30b362edca97eb63b382ef5f621b575f11b5b50b
Aniket <63105008+iam-aniket@users.noreply.github.com>
2020-11-09 07:46:56 +0530

    Left + new Merge middle + Right all
    Example
    Given [1,2],[3,5],[6,7],[8,10],[12,16], insert and merge [4,9] in as [1,2],[3,10],[12,16]
    
    ![image](https://user-images.githubusercontent.com/63105008/98493067-91e6ed80-225f-11eb-8b2e-a9a1228a096d.png)

---
+++ Logic/Insert Interval.cpp
@@ -0,0 +1,38 @@
+#define pb push_back
+#define rep(i,a,n) for(int i = a; i < n; i++)
+class Solution {
+public:
+	vector<vector<int>> insert(vector<vector<int>>& a, vector<int>& x)
+	{
+		vector<vector<int>> ans;
+		int n = a.size(),i = 0;
+		
+        // add all the intervals ending before newInterval starts
+        // Left part (no intersection with newInterval)
+        while (i < n && a[i][1] < x[0])
+		{
+			ans.pb(a[i]);
+			i++;
+		}
+        
+        // merge all overlapping intervals to one considering newInterval
+		// newInterval part (with or without merge)
+        while (i < n && a[i][0] <= x[1])
+		{
+			x[0] = min(a[i][0], x[0]);
+			x[1] = max(a[i][1], x[1]);
+			i++;
+		}
+		
+        ans.pb(x);// add the union of intervals we got
+        
+		// add all the rest - since they are defintely larger
+        // Right part (no intersection with newInterval)
+        while (i < n)
+		{
+			ans.pb(a[i]);
+			i++;
+		}
+		return ans;
+	}
+};
