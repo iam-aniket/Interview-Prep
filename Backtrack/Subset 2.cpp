922d1355d6de013ae3d10d1d0f8ca82d50618b70
Aniket <63105008+iam-aniket@users.noreply.github.com>
2020-11-18 12:11:29 +0530

    https://leetcode.com/problems/subsets/discuss/27281/A-general-approach-to-backtracking-questions-in-Java-(Subsets-Permutations-Combination-Sum-Palindrome-Partitioning)
    REFER this - general BT
    https://leetcode.com/problems/subsets/discuss/27281/A-general-approach-to-backtracking-questions-in-Java-(Subsets-Permutations-Combination-Sum-Palindrome-Partitioning)

---
+++ Backtrack/Subset 2.cpp
@@ -0,0 +1,32 @@
+#define rep(i,a,n) for(int i = a; i < n; i++)
+#define pb push_back
+class Solution {
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& a) 
+    {
+        vector<vector<int>> ans;
+        
+        sort(a.begin(),a.end());
+        
+        vector<int> temp;
+        
+        backtrack(ans,temp,a,0);
+        
+        return ans;
+    }
+    void backtrack(vector<vector<int>>& ans,vector<int> temp,vector<int> a,int start)
+    {
+        ans.pb(temp);
+        
+        for(int i = start;i < a.size();i++)
+        {
+            if(i > start && a[i] == a[i-1])continue; 
+            
+            temp.pb(a[i]);
+        
+            backtrack(ans,temp,a,i+1);
+            
+            temp.pop_back();
+        }
+    }
+};
